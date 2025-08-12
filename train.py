4ad22c458a1063c7283e46301cdf56af8676cdea
peteksnr <117526538+peteksnr@users.noreply.github.com>
2025-08-12 22:13:17 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
-model = YOLO("/Users/peteksener/Desktop/y/ultralytics/ultralytics/cfg/models/11/eye.yaml")
-results = model.train(model=model, data="/Users/peteksener/Desktop/y/ultralytics/ultralytics/cfg/datasets/eyes.yaml", epochs=10, imgsz=614, batch=8, device='mps')
+model = YOLO("path/to/eye.yaml")
+results = model.train(model=model, data="path/to/eyes.yaml", epochs=10, imgsz=614, batch=8, device='mps')
