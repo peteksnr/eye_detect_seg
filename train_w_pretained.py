21b62a5e0f677e122b65155e8735240f99254701
peteksnr <117526538+peteksnr@users.noreply.github.com>
2025-08-12 22:13:51 +0300

    Update train_w_pretained.py

--- train_w_pretained.py
+++ train_w_pretained.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 
-model = YOLO("/Users/peteksener/Desktop/y/runs/detect/train9/weights/best.pt")
-results = model.train(model=model, data="/Users/peteksener/Desktop/y/ultralytics/ultralytics/cfg/datasets/eyes.yaml", epochs=20, imgsz=614, batch=8, device='mps')
+model = YOLO("path/to/pretrained/segmentation/model")
+results = model.train(model=model, data="path/to/eyes.yaml", epochs=20, imgsz=614, batch=8, device='mps')
